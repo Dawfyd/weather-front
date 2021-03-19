1e9adb5e1391895d8c32ade336f5c717fe159935
Dawfyd <>
2021-03-19 02:41:09 -0500

    avance maquetacion

--- src/app/components/forecast/forecast.jsx
+++ src/app/components/forecast/forecast.jsx
@@ -1,5 +1,48 @@
 import React from "react";
-
-const Forecast = () => <div className="container_forecast">hola</div>;
+import rain from "../../../assets/rain.svg";
+const Forecast = () => (
+  <div className="container_forecast">
+    <div className="container_forecast_flexbox">
+      <div className="container_forecast_text">
+        <span className="text_forecast_title">3 Days</span>
+        <span className="text_forecast">Forecast</span>
+      </div>
+      <div className="container_items_flex">
+        <div className="items_flex_icon">
+          <img src={rain} className="icon_weather" alt="icon_weather" />
+        </div>
+        <div className="items_flex_weather">
+          <div className="text_item_day">Friday</div>
+          <div className="text_item_weather">Rain</div>
+        </div>
+        <div className="items_flex_card"> 32°/23°</div>
+      </div>
+      <div className="container_items_flex">
+        {" "}
+        <div className="items_flex_icon">
+          <img src={rain} className="icon_weather" alt="icon_weather" />
+        </div>
+        <div className="items_flex_weather">
+          {" "}
+          <div className="text_item_day">Friday</div>
+          <div className="text_item_weather">Rain</div>
+        </div>
+        <div className="items_flex_card_next"> 34°/24°</div>
+      </div>
+      <div className="container_items_flex">
+        {" "}
+        <div className="items_flex_icon">
+          <img src={rain} className="icon_weather" alt="icon_weather" />
+        </div>
+        <div className="items_flex_weather">
+          {" "}
+          <div className="text_item_day">Friday</div>
+          <div className="text_item_weather">Rain</div>
+        </div>
+        <div className="items_flex_card_next"> 31°/23°</div>
+      </div>
+    </div>
+  </div>
+);
 
 export default Forecast;
--- src/app/components/places/places.jsx
+++ src/app/components/places/places.jsx
@@ -1,5 +1,62 @@
 import React from "react";
+import icon_location_white from "../../../assets/icon_location_white.svg";
+import avatar_1 from "../../../assets/avatars/ava1.png";
+import avatar_2 from "../../../assets/avatars/ava2.png";
+import avatar_3 from "../../../assets/avatars/ava3.png";
+import { Avatar } from "antd";
 
-const Forecast = () => <div className="container_places">hola</div>;
+const Forecast = () => (
+  <div className="container_places">
+    <div className="container_places_flexbox">
+      <p className="text_places_title">Place to</p>
+      <p className="text_places">Visit</p>
+      <div className="item_reviews">
+        <p className="text_add_review">+</p>
+        <p className="text_reviews">Top Reviews</p>
+        <div className="item_reviews_avatars">
+          <Avatar shape="square" className="avatar" src={avatar_1} />
+          <Avatar shape="square" className="avatar" src={avatar_2} />
+          <Avatar shape="square" className="avatar" src={avatar_3} />
+          <Avatar
+            shape="square"
+            className="avatar"
+            style={{
+              backgroundColor: "#b5a8f8",
+            }}
+          >
+            +3
+          </Avatar>
+        </div>
+      </div>
+      <div className="container_img_places">
+        <img
+          src={icon_location_white}
+          className="icon_location_white"
+          alt="icon_location"
+        />
+        <span className="text_location_visit">Arab Street Singapore</span>
+      </div>
+      <div className="container_flex_img">
+        <div className="container_img_places_short">
+          <img
+            src={icon_location_white}
+            className="icon_location_white_short"
+            alt="icon_location"
+          />
+          <span className="text_location_visit_short">Art Science Museum</span>
+        </div>
+        <div className="container_new_places">
+          <img
+            src={icon_location_white}
+            className="icon_location_white_new"
+            alt="icon_location"
+          />
+          <span className="text_location_visit_new">Fountain of Wealth</span>
+          <button className="button_new_location">+</button>
+        </div>
+      </div>
+    </div>
+  </div>
+);
 
 export default Forecast;
